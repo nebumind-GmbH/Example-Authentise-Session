996790d8eebcb19f2ae0b7c96203e547feefb3f3
Far McKon <far@authentise.com>
2019-10-15 16:21:41 -0400

    Pseudocode continued for uploading models

--- MakeOrderExample.py
+++ MakeOrderExample.py
@@ -10,13 +10,25 @@ def upack_zip(filename):
         metadata["files"].append(fake_file_dict)
 
 
-def api_dict_from_zip_dict(zip_dict):
+def model_dict_from_zip(zip_dict):
+    """ TBD. unpack zip metadata into model-centric details"""
     return {}
+
+
+def line_item_dict_from_zip(zip_dict):
+    """ TBD. unpack zip metadata into line-item centric details"""
+    return {}
+
+
+def order_dict_from_zip(zip_dict):
+    """ TBD. unpack zip metadata into order centric details"""
+    return
 
 
 if __name__ == "__main__":
     print("Running Make Order Example at the command line")
 
+    # build our command line parser
     parser = argparse.ArgumentParser(
         description="Example of getting an API Key for Authentise."
     )
@@ -28,15 +40,28 @@ if __name__ == "__main__":
 
     sesh = None
     if "username" in args and "password" in args:
-        # print(args)
         sesh = AuthentiseSession(host="authentise.com", verify_ssl=True)
         sesh.init_api(args.username, args.password)
 
+    if not sesh:
+        LOGGER.error("failed to make a session ")
+        exit(1)
+
+    # unpack the data from the zip
     models, metadata = [], {}  # empty entries
-    if ("zip_filename" in args) and sesh:
+    if "zip_filename" in args:
         models, metadata = unpack_zip(args.zip_filename)
 
+    line_item_uris = []
+    # for each model, load it, push it to the
     if models and metadata:
         for model in models:
             model_dict = api_dict_from_zip_dict()
-            sesh.post_and_upload("/model/", model, model_dict)
+            model_uri = sesh.post_and_upload("/model/", model, model_dict)
+            line_item_dict = line_item_dict_from_zip_dict(zip_dict)
+            line_item_dict["model"] = model_uri
+            line_item_uri = sesh.put("/line-tem/", line_item_dict)
+            line_item_uris.append(line_item_uri)
+    order_dict = order_dict_from_zip(zip_dict)
+    order_dict["line-items"] = line_item_uri
+    order_uri = sesh.put("/order/", order_dict)
